851bde0f7aeb8284ef07d35d8f5cdc24bc8b4df7
Scott Jackson <jackson.scott.m@gmail.com>
2022-03-21 21:01:07 -0400

    Added part 2 solution

--- day2/kt2/src/main/kotlin/Main.kt
+++ day2/kt2/src/main/kotlin/Main.kt
@@ -7,7 +7,7 @@ enum class Direction() {
     DOWN
 }
 
-data class Command (val dir: Direction, val amount: Int)
+data class Command(val dir: Direction, val amount: Int)
 
 fun readInputFile(filename: String): List<String> {
     val inputStream: InputStream = File(filename).inputStream()
@@ -23,24 +23,54 @@ fun main(args: Array<String>) {
     val rawData = readInputFile(args[0])
     var commands = ArrayDeque<Command>()
 
+    // Build command list
     rawData.forEach {
         val item = it.split(" ")
         val x = Command(Direction.valueOf(item[0].uppercase()), item[1].toInt())
         commands.add(Command(Direction.valueOf(item[0].uppercase()), item[1].toInt()))
     }
 
+    println("Found ${commands.size} commands")
+
+    partOne(commands)
+    partTwo(commands)
+}
+
+fun partOne(cmds: ArrayDeque<Command>): Unit {
     var depth = 0
     var pos = 0
 
-    commands.forEach() {
-        when(it.dir) {
+    println("-- Part One --")
+
+    cmds.forEach() {
+        when (it.dir) {
             Direction.FORWARD -> pos = pos + it.amount
             Direction.UP -> depth = depth - it.amount
             Direction.DOWN -> depth = depth + it.amount
         }
     }
 
-    println("Found ${commands.size} commands")
+    println("Depth: ${depth}   Position: ${pos}")
+    println("Product: ${depth * pos}\n\n")
+}
+
+fun partTwo(cmds: ArrayDeque<Command>): Unit {
+    var depth = 0
+    var pos = 0
+    var aim = 0
+
+    println("-- Part Two --")
+    cmds.forEach() {
+        when (it.dir) {
+            Direction.FORWARD -> {
+                pos = pos + it.amount
+                depth = depth + (aim * it.amount)
+            }
+            Direction.UP -> aim = aim - it.amount
+            Direction.DOWN -> aim = aim + it.amount
+        }
+    }
+
     println("Depth: ${depth}   Position: ${pos}")
     println("Product: ${depth * pos}")
 }
